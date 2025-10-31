47498b1eb045d68bb0caeed6c63790050a964a3e
FinkEmis <finkx71@gmail.com>
2025-10-31 17:37:22 -0300

    [ Mudanças no produto ]
    ps: codigos sobre a classe fornecedor então comentados pela falta do fornecedor.

--- src/main/java/br/unip/ads/pim/meuhortifruti/dto/ProdutoRequestDTO.java
+++ src/main/java/br/unip/ads/pim/meuhortifruti/dto/ProdutoRequestDTO.java
@@ -1,21 +1,20 @@
 package br.unip.ads.pim.meuhortifruti.dto;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProdutoRequestDTO {
+
     @NotBlank(message = "O nome do produto é obrigatório")
     @Size(min = 3, max = 300, message = "O nome deve ter entre 3 até 100 caracteres")
     private String nome;
@@ -24,7 +23,13 @@ public class ProdutoRequestDTO {
     @DecimalMin(value = "0.01", message = "O preço deve ser maior que zero")
     private BigDecimal preco;
 
-    @NotNull(message = "O preço do produto é obrigatório")
+    @NotNull(message = "A quantidade de produto é obrigatório")
     @DecimalMin(value = "A quantidade não pode ser negativa")
     private Integer quantidadeEstoque;
+
+    @NotNull
+    private LocalDate dataEntrega;
+
+    @Future(message = "Data de validade deverá ser futura")
+    private LocalDate dtValidade;
 }
